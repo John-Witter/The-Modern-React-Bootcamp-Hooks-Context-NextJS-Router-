622844476db281ca5f07afba513ae1d9def7c1f9
John-Witter <mrsolginski@gmail.com>
2021-08-18 12:13:41 -0400

    Section 32 CounterClass.js

---
+++ Section 32 Introducing React Hooks/CounterClass.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CounterClass extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Count Is: </h1>
+                <button>Add 1</button>
+            </div>
+        )
+    }
+}
+export default CounterClass
